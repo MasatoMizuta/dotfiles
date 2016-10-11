ccde77ac6f28fff41891e7a20469ca7b3f8fa6cb
Masato Mizuta <mst.mizuta@gmail.com>
2016-10-11 00:32:38 -0400

    Change oh-my-zsh installation to use git clone

--- zsh.sh
+++ zsh.sh
@@ -1,12 +1,13 @@
 #!/usr/sh
 
 # Insall Oh My Zsh
-if [ ! -e $HOME/.oh-my-zsh ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ZSH=~/.oh-my-zsh
+if [ ! -e $ZSH ]; then
+    git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH
 fi
 
 # Install zsh-syntax-highlighting
-ZSH_SYNTAX_DIR=$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-if [ ! -e $ZSH_SYNTAX_DIR ]; then
-    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_SYNTAX_DIR
+ZSH_SYNTAX=$ZSH/custom/plugins/zsh-syntax-highlighting
+if [ ! -e $ZSH_SYNTAX ]; then
+    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_SYNTAX
 fi
